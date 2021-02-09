07359bb430073f93d0a1c085ab15fe12a4cb8361
VinodhS577 <75975228+VinodhS577@users.noreply.github.com>
2021-02-09 17:56:17 +0530

    Add files via upload
    Added Pipeline build string in Java file

--- Test.java
+++ Test.java
@@ -3,6 +3,6 @@
  public class Test {
 	public static void main(String args[])
 	{
-		 System.out.println("Jenkins Build done Successfully !!!");
+		 System.out.println("Jenkins Pipeline Build done Successfully !!!");
 	}
 }
